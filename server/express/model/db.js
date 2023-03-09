5e0aca4aa47e2e38880aed42707effc2c0fcb48e
David Chamberlain <david.ross.chamberlain@gmail.com>
2023-03-09 16:10:31 +0100

    updated

--- server/express/model/db.js
+++ server/express/model/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const URL = "mongodb://127.0.0.1:27017/polyglot_db";
+const URL = 'mongodb://localhost:27017/polyglot'
 try {
   mongoose.connect(URL);
   console.log("connection to database was successful");
